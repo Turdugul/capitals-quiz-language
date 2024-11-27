f93ec4c34d4ad092bbab9b93012862ec3222904a
Turdugul <okonbaevaturdugul@gmail.com>
2024-11-27 00:07:58 +0000

    deploy

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -50,7 +50,7 @@ const Questions: React.FC<QuestionProps> = ({
       <div className="space-y-2">
         {options.map((option) => {
           let buttonClass =
-            "bg-blue-500 hover:bg-blue-700 dark:bg-blue-400 dark:hover:bg-blue-600";
+            "bg-blue-500 md:hover:bg-blue-700 dark:bg-blue-400 dark:md:hover:bg-blue-600";
           if (selectedAnswer) {
             if (option === country.capital) buttonClass = "bg-green-500";
             else if (option === selectedAnswer) buttonClass = "bg-red-500";
